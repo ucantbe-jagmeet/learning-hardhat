18d2f303585c0e30d2d847b930cc217b9bb5050e
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-23 05:32:25 +0530

    testing of contract using mocha framework and ethers lib part B

--- by-code-eater/test/token.js
+++ by-code-eater/test/token.js
@@ -16,4 +16,24 @@ describe("TokenTest contract", () => {
 
     expect(ownerBalance.toNumber()).to.equal(tokenSupply); // token supply = 10000
   });
+
+  it("Should transfer tokens between accounts", async () => {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("TokenTest"); // instance of contract
+
+    const hardhatToken = await Token.deploy(); // deploy contract
+
+    //transfer 10 tokens from owner to addr1
+    await hardhatToken.transfer(addr1.address, 10);
+    expect((await hardhatToken.balanceOf(addr1.address)).toNumber()).to.equal(
+      10
+    );
+
+    //transfer 10 tokens from add1 to addr2
+    await hardhatToken.connect(addr1).transfer(addr1.address, 10);
+    expect((await hardhatToken.balanceOf(addr1.address)).toNumber()).to.equal(
+      10
+    );
+  });
 });
